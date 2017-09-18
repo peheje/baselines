5d7b8ea0a0dd700f7e5332a20523ee47d751f540
Peter <peterhj@gmail.com>
2017-09-18 13:06:10 +0200

    i need this?

--- baselines/deepq/experiments/traci/agents/train_traffic_deep.py
+++ baselines/deepq/experiments/traci/agents/train_traffic_deep.py
@@ -1,3 +1,10 @@
+import os, sys
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+    sys.path.append(tools)
+else:
+    sys.exit("please declare environment variable 'SUMO_HOME'")
+
 import gym
 
 from baselines import deepq
