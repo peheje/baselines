339eee268e6b9d7880f36fbbb0dd9d21ccb85d80
Unknown <nikolajholden@gmail.com>
2017-11-30 14:23:14 +0100

    enjoy traffic (webster) now with rising car probs

--- traci/agents/enjoy_traffic_cycle.py
+++ traci/agents/enjoy_traffic_cycle.py
@@ -21,18 +21,20 @@ def main():
     env.configure_traci(perform_actions=False,
                         num_car_chances=1000,
                         start_car_probabilities=[0.25, 0.05],
+                        end_car_probabilities=[1.0,0.1],
                         enjoy_car_probs=False,
                         reward_func=env.reward_total_waiting_vehicles,
                         action_func=BaseTraciEnv.set_light_phase_4_cross_green_dir,
                         state_contain_num_cars_in_queue_history=True,
                         state_contain_time_since_tl_change=True,
                         state_contain_tl_state_history=True,
+                        num_episodes_from_start_car_probs_to_end_car_probs=100,
                         state_contain_avg_speed_between_detectors_history=False,
                         num_actions_pr_trafficlight=2)
 
     # env.render()
     _, done = env.reset(), False
-    for episode in range(5):
+    for episode in range(100):
         while not done:
             _, _, done, _ = env.step(-1)
         _, done = env.reset(), False
